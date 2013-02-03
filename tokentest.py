be833235c5f28cd92f4d2867fb2a54208a393737
Jared Wong <jaredlwong@gmail.com>
2013-02-02 23:45:32 -0500

    added dict

--- tokentest.py
+++ tokentest.py
@@ -34,11 +34,68 @@ map(lambda x:reserved.add(x), [
 "continue","finally","is","return",
 "def","for","lambda","try"])
 
-for t in tokenize.generate_tokens(f.readline):
-    if tok_name[t[0]] == 'NAME':
-        if t[1] not in reserved:
-            names.add(t[1])
-            print(t[1])
+tok_names = {0: 'ENDMARKER',
+ 256: 'NT_OFFSET',
+ 2: 'NUMBER',
+ 3: 'STRING',
+ 4: 'NEWLINE',
+ 5: 'INDENT',
+ 6: 'DEDENT',
+ 7: 'LPAR',
+ 8: 'RPAR',
+ 9: 'LSQB',
+ 10: 'RSQB',
+ 11: 'COLON',
+ 12: 'COMMA',
+ 13: 'SEMI',
+ 14: 'PLUS',
+ 15: 'MINUS',
+ 16: 'STAR',
+ 17: 'SLASH',
+ 18: 'VBAR',
+ 19: 'AMPER',
+ 20: 'LESS',
+ 21: 'GREATER',
+ 22: 'EQUAL',
+ 23: 'DOT',
+ 24: 'PERCENT',
+ 25: 'BACKQUOTE',
+ 26: 'LBRACE',
+ 27: 'RBRACE',
+ 28: 'EQEQUAL',
+ 29: 'NOTEQUAL',
+ 30: 'LESSEQUAL',
+ 31: 'GREATEREQUAL',
+ 32: 'TILDE',
+ 33: 'CIRCUMFLEX',
+ 34: 'LEFTSHIFT',
+ 35: 'RIGHTSHIFT',
+ 36: 'DOUBLESTAR',
+ 37: 'PLUSEQUAL',
+ 38: 'MINEQUAL',
+ 39: 'STAREQUAL',
+ 40: 'SLASHEQUAL',
+ 41: 'PERCENTEQUAL',
+ 42: 'AMPEREQUAL',
+ 43: 'VBAREQUAL',
+ 44: 'CIRCUMFLEXEQUAL',
+ 45: 'LEFTSHIFTEQUAL',
+ 46: 'RIGHTSHIFTEQUAL',
+ 47: 'DOUBLESTAREQUAL',
+ 48: 'DOUBLESLASH',
+ 49: 'DOUBLESLASHEQUAL',
+ 50: 'AT',
+ 1: 'NAME',
+ 52: 'ERRORTOKEN',
+ 53: 'N_TOKENS',
+ 51: 'OP'}
+
+def analyze():
+    for t in tokenize.generate_tokens(f.readline):
+        if tok_name[t[0]] == 'NAME':
+            if t[1] not in reserved:
+                names.add(t[1])
+                print(t[1])
 tmp = 0
 for w in names:
     tmp += len(w)
